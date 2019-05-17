d0494fbd98c972114a0e25fe02b234f18459503d
vinoth.m <vinoth.m@esecdev.com>
2019-05-17 12:20:36 +0530

    added remote db

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,8 +1,25 @@
 const mysql = require('mysql');
+var env = process.env.NODE_ENV || 'development';
+console.log("mode:"+env);
+if (env === "development") {
+    module.exports = function () {
+        return mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'sticker'
+        });
+    };
+} else {
+    module.exports = function () {
+        return mysql.createConnection({
+            host: 'remotemysql.com',
+            user: '29Jlzge8OP',
+            password: 'vSMXTg3BAq',
+            database: '29Jlzge8OP'
+        });
+    };
+}
 
-module.exports = () => mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'sticker'
-});
+
+
